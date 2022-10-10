e773e2b63a725685a3104944bea3127332d0eaac
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-10-10 15:32:41 +0600

    initial commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Product.css'
+
+const Product = (props) => {
+
+    const {name, img, seller, price, ratings}=props.product;
+
+    const {handleAddToCart} =props;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller:{seller}</small></p>
+                <p><small>Rating:{ratings}</small></p>
+            </div>
+            <button onClick={handleAddToCart} className='btm-cart'>
+                <p>Add to Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
